651dbfb74d6abcbad5e670f3170ad8e599eb7a5c
Arm <69001162+arskiir@users.noreply.github.com>
2022-05-30 01:10:17 +0700

    🚧 make the entire app no ssr

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
+import dynamic from 'next/dynamic'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default dynamic(() => Promise.resolve(MyApp), {
+  ssr: false,
+})
