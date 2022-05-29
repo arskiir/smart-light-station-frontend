c4701eb821a96823a6b1f84fdd82f2fc65de1e65
Arm <69001162+arskiir@users.noreply.github.com>
2022-05-30 00:44:42 +0700

    💡 add a comment

--- utils/getDeviceState.ts
+++ utils/getDeviceState.ts
@@ -1,5 +1,9 @@
 import { DeviceState } from './../types/device-state'
 
+/**
+ * Get device state from Netpie
+ * @returns The device state
+ */
 export const getDeviceState = async (): Promise<DeviceState> => {
   // TODO: Implement this function
   return {
