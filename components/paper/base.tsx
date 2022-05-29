f9708cd714664d594b91d1418f3c687c05ed2390
Arm <69001162+arskiir@users.noreply.github.com>
2022-05-30 02:44:43 +0700

    🚧

--- components/paper/base.tsx
+++ components/paper/base.tsx
@@ -9,7 +9,7 @@ const Paper: FC<{ title: string; value?: number | string; img: string }> = ({
     <div className="relative inline-block">
       <div className="absolute w-full h-full flex flex-col justify-center items-center gap-y-4">
         <p className="text-4xl whitespace-pre-wrap">{title}</p>
-        <p className="text-[#A81D1D] text-5xl">{value ?? '-'}</p>
+        <p className="text-[#A81D1D] text-5xl">{value ?? ''}</p>
       </div>
       <img src={img} alt="" className="w-44" />
     </div>
