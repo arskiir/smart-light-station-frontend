b767086f2d02b0a9fa1a20530df7f56af1c988f3
Arm <69001162+arskiir@users.noreply.github.com>
2022-05-30 17:16:06 +0700

    💄 tween ldr value

--- components/paper/LDR.tsx
+++ components/paper/LDR.tsx
@@ -1,9 +1,25 @@
-import { FC } from 'react'
+import { FC, useEffect, useState } from 'react'
 import { DeviceState } from '../../types/device-state'
 import Paper from './base'
+import { TweenLite } from 'gsap'
 
-const LDRPaper: FC<{ value?: DeviceState['LDR'] }> = ({ value }) => {
-  return <Paper img="/paper_ldr.png" title="LDR" value={value}></Paper>
+const valueObj = {
+  value: 1,
+}
+
+const LDRPaper: FC<{ value?: DeviceState['LDR'] }> = ({ value = 1 }) => {
+  const [tweenValue, setTweenValue] = useState(value)
+
+  useEffect(() => {
+    TweenLite.to(valueObj, 0.5, {
+      value,
+      onUpdate: () => {
+        setTweenValue(Math.round(valueObj.value))
+      },
+    })
+  }, [value])
+
+  return <Paper img="/paper_ldr.png" title="LDR" value={tweenValue}></Paper>
 }
 
 export default LDRPaper
