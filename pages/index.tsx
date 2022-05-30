e2562844fbddb190c735909dbe4107990133116c
Arm <69001162+arskiir@users.noreply.github.com>
2022-05-30 16:08:16 +0700

    หกด

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,6 +23,7 @@ export default function Home() {
       >
         {statusText}
       </span>
+
       <div
         className="bg-black w-full h-full absolute"
         style={{
@@ -34,6 +35,7 @@ export default function Home() {
         state={state}
         className="absolute top-1/2 -translate-y-1/5 left-1/2 -translate-x-1/2"
       />
+
       <img
         src="/lamp.png"
         alt="lamp"
