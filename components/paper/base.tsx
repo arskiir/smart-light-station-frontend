0957ac15203a62a0c32ea082521b7818dfeddc1c
Arm <69001162+arskiir@users.noreply.github.com>
2022-05-31 01:56:49 +0700

    💄 more shadow when hover paper

--- components/paper/base.tsx
+++ components/paper/base.tsx
@@ -7,7 +7,7 @@ const Paper: FC<{
   className?: string
 }> = ({ title, value, img, className = '' }) => {
   return (
-    <div className={`relative h-fit shadow-lg ${className}`}>
+    <div className={`relative h-fit shadow-lg hover:shadow-xl ${className}`}>
       <div className="absolute w-full h-full flex flex-col justify-center items-center gap-y-4">
         <p className="text-4xl whitespace-pre-wrap">{title}</p>
         <p className="text-[#A81D1D] text-5xl">{value ?? ''}</p>
