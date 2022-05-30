fbf80780c77f06c3f635139f6bb40eae39749a48
Arm <69001162+arskiir@users.noreply.github.com>
2022-05-30 22:34:32 +0700

    💄 set page title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import PaperSet from '../components/PaperSet'
 import { useDeviceState } from '../hooks/useDeviceState'
+import Head from 'next/head'
 import { getBgOpacity } from '../utils/getBgOpacity'
 import { getStatusText } from '../utils/getStatusText'
 
@@ -17,45 +18,50 @@ export default function Home() {
   }, [state])
 
   return (
-    <main className="w-screen h-screen overflow-hidden relative">
-      <span
-        className={`font-depixel text-5xl absolute z-10 top-[37%] left-1/2 -translate-x-1/2 transition-colors ${
-          state.LightStatus === 1
-            ? 'text-black'
-            : bgOpacity > 0.5
-            ? 'text-white'
-            : 'text-black'
-        }`}
-      >
-        {statusText}
-      </span>
+    <>
+      <Head>
+        <title>Smart Light Station</title>
+      </Head>
+      <main className="w-screen h-screen overflow-hidden relative">
+        <span
+          className={`font-depixel text-5xl absolute z-10 top-[37%] left-1/2 -translate-x-1/2 transition-colors ${
+            state.LightStatus === 1
+              ? 'text-black'
+              : bgOpacity > 0.5
+              ? 'text-white'
+              : 'text-black'
+          }`}
+        >
+          {statusText}
+        </span>
 
-      <div
-        className="bg-black w-full h-full absolute transition-opacity"
-        style={{
-          opacity: bgOpacity,
-          transitionDuration: `${updateInterval * 0.5}ms`,
-        }}
-      ></div>
+        <div
+          className="bg-black w-full h-full absolute transition-opacity"
+          style={{
+            opacity: bgOpacity,
+            transitionDuration: `${updateInterval * 0.5}ms`,
+          }}
+        ></div>
 
-      {state.LightStatus === 1 ? (
-        <img
-          src="/lamp_on.png"
-          alt="lamp"
-          className="left-1/2 -translate-x-1/2 top-0 absolute"
+        {state.LightStatus === 1 ? (
+          <img
+            src="/lamp_on.png"
+            alt="lamp"
+            className="left-1/2 -translate-x-1/2 top-0 absolute"
+          />
+        ) : (
+          <img
+            src="/lamp.png"
+            alt="lamp"
+            className="left-1/2 -translate-x-1/2 top-0 absolute"
+          />
+        )}
+
+        <PaperSet
+          state={state}
+          className="absolute top-1/2 -translate-y-1/5 left-1/2 -translate-x-1/2"
         />
-      ) : (
-        <img
-          src="/lamp.png"
-          alt="lamp"
-          className="left-1/2 -translate-x-1/2 top-0 absolute"
-        />
-      )}
-
-      <PaperSet
-        state={state}
-        className="absolute top-1/2 -translate-y-1/5 left-1/2 -translate-x-1/2"
-      />
-    </main>
+      </main>
+    </>
   )
 }
